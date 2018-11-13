0a19f2bdb60708414856481afdb9526509c85a7a
JulesBelveze <jules.belveze@hotmail.fr>
2018-11-13 19:40:21 +0100

    Page rank + test page rank

---
+++ testers/PageRankTest.py
@@ -0,0 +1,23 @@
+import networkx as nx
+import sys
+sys.path.append('..')
+import PageRank
+
+G = nx.DiGraph()
+
+G.add_nodes_from([1,2,3,4,5])
+
+G.add_edge(1,2)
+G.add_edge(1,4)
+G.add_edge(1,3)
+G.add_edge(2,3)
+G.add_edge(4,3)
+G.add_edge(1,5)
+G.add_edge(2,1)
+G.add_edge(5,2)
+G.add_edge(3,4)
+G.add_edge(2,4)
+
+PR = PageRank.PageRank(G)
+PR.constructDispersionMatrix(G)
+print(PR.getPageRank())
